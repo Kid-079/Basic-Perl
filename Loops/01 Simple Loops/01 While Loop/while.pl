83d6c410595c189d299bff3e9b0f86f75610f454
nuKidd <92396121+nuKidd@users.noreply.github.com>
2021-10-13 03:52:29 +0900

    Add files via upload

---
+++ Loops/01 Simple Loops/01 While Loop/while.pl
@@ -0,0 +1,16 @@
+# #!/usr/local/bin/perl
+
+# $a = 0;
+
+# # While Loop
+# while( $a < 10 ) {
+# 	print "Value of a : $a\n";
+# 	$a = $a + 1;
+# }
+
+
+# CATATAN :
+# RUMUS :
+# while(condition) {
+# 	statement(s);
+# }
