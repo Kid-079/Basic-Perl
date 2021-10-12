1906209fbb30fef89189b31b16309be9e48998d3
nuKidd <92396121+nuKidd@users.noreply.github.com>
2021-10-13 03:40:48 +0900

    Delete Perl directory

--- Perl/Subroutines/01 Define and Call/subroutines.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-# Function Definition
-sub Hello {
-	print "Hello World\n";
-}
-
-# Function Call
-Hello();
--- Perl/Subroutines/06 Private Variables/outFunc.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-# Global Variable
-$string = "Hello World.!!";
-
-# Function Call
-print "Outside The Function $string\n";
-
