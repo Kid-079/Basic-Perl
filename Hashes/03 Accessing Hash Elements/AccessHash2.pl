d99500ea795e2962943683e607d27c210680c26f
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-18 19:29:52 +0700

    Add files via upload

---
+++ Hashes/03 Accessing Hash Elements/AccessHash2.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+%nama = ('Caramel' => 108, 'Creamy' => 107, 'Cincau' => 78, 'Tiramisu', => 97, 'Mochi', => 66);
+#%nama = ('Caramel' => 114, 'Creamy' => 111, 'Cincau' => 83, 'Tiramisu', => 115, 'Mochi', => 79);
+
+print "$nama{'Caramel'}\n";
+print "$nama{'Creamy'}\n";
+print "$nama{'Cincau'}\n";
+print "$nama{'Tiramisu'}\n";
+print "$nama{'Mochi'}\n";
