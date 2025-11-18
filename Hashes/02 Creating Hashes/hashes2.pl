29dd9f4fcd22d75f5d8c6996911c57817c7c8c69
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-18 19:29:33 +0700

    Add files via upload

---
+++ Hashes/02 Creating Hashes/hashes2.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+%kata = ('Tailor' => 78, 'Butter'=> 105, 'Gazelle'=> 71,'Oil'=> 80, 'Puppet'=> 110, 'Hood'=> 65);
+#%kata = ('Tailor'=> 69, 'Butter'=> 97, 'Gazelle'=> 65,'Oil'=> 112, 'Puppet'=> 107, 'Hood'=> 76);
+#%kata = ('Tailor'=> 107, 'Butter'=> 78, 'Gazelle'=> 101,'Oil'=> 72, 'Puppet'=> 119, 'Hood'=> 117);
+
+print "\$kata{'Tailor'} = $kata{'Tailor'}\n";
+print "\$kata{'Butter'} = $kata{'Butter'}\n";
+print "\$kata{'Gazelle'} = $kata{'Gazelle'}\n";
+print "\$kata{'Oil'} = $kata{'Oil'}\n";
+print "\$kata{'Puppet'} = $kata{'Puppet'}\n";
+print "\$kata{'Puppet'} = $kata{'Hood'}\n";
