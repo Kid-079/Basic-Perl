250ab93a5416799022ed76414b962fcd73c2faa1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 14:15:33 +0700

    creation.pl

--- Arrays/02 Array Creation/1/creation.pl
+++ Arrays/02 Array Creation/1/creation.pl
@@ -1,10 +1,20 @@
 #!/usr/bin/perl
 
 @array1 = (1, 2, 'Kumar', 'Salaman', 'Anjeli', 'Damai');
-@array2 = qw/Ini adalah Sebuah Array/;
+#@array2 = (1, 2, 3, 4, 5, 'Vampire', 'Dracule', 'Wicked', 'Ghost');
+@array3 = qw/Ini adalah Sebuah Array/;
 
 print "\$array1[0] = $array1[2]\n";
 print "\$array1[0] = $array1[4]\n";
 print "\$array1[0] = $array1[3]\n";
 print "\$array1[0] = $array1[5]\n";
+
+#print "\$array2[0] = $array2[3]\n";
+#print "\$array2[0] = $array2[8]\n";
+#print "\$array2[0] = $array2[1]\n";
+#print "\$array2[0] = $array2[5]\n";
+#print "\$array2[0] = $array2[4]\n";
+#print "\$array2[0] = $array2[7]\n";
+#print "\$array2[0] = $array2[0]\n";
+#print "\$array2[0] = $array2[6]\n";
 
