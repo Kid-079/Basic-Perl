32b6eb1b4dbc479aeb9010cad379fcd795c734e1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 13:08:38 +0700

    simpleArray.pl

--- Arrays/01 Simple Arrays/simpleArray.pl
+++ Arrays/01 Simple Arrays/simpleArray.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
 @num = (1, 2, 3);
+#@num = (1, 2, 3, 4, 5);
 @text = ("Hello", "hello", "HELLO");
+#@vocab = ("Vampire", "Dracula", "Avatar", "Wicked", "Ghost");
 
 print "\$num[0] = $num[0]\n";
 print "\$num[1] = $num[1]\n";
@@ -9,3 +11,15 @@ print "\$num[2] = $num[2]\n";
 print "\$Text[0] = $text[0]\n";
 print "\$Text[1] = $text[1]\n";
 print "\$Text[2] = $text[2]\n";
+
+#print "\$num[0] = $num[0]\n";
+#print "\$num[1] = $num[1]\n";
+#print "\$num[2] = $num[2]\n";
+#print "\$num[3] = $num[3]\n";
+#print "\$num[4] = $num[4]\n"
+#print "\$Text[0] = $text[0]\n";
+#print "\$Text[1] = $text[1]\n";
+#print "\$Text[2] = $text[2]\n";
+#print "\$Text[3] = $text[3]\n";
+#print "\$Text[4] = $text[4]\n";
+
