769b8c7d20df18d8c52a251eb79a91f7b05d67c1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 12:23:22 +0700

    check.pl

--- Hashes/06 Checking for Existence/check.pl
+++ Hashes/06 Checking for Existence/check.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
 %data = ('A' => 48, 'B' => 49, 'C' => 50);
+#%data = ('A' => 48, 'B' => 49, 'C' => 50, 'D' => 51, 'E' => 52);
 
 if ( exists($data{'D'}) ) {
 	print "Data A memiliki nilai : $data{'A'}\n";
+	#print "Data A memiliki nilai : $data{'E'}\n";
 } else {
 	print "Check Variabel.!!\n";
 }
