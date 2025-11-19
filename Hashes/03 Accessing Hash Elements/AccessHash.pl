543f46aaf5e7ce2c7666c123defe2819acd4c86e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 12:18:47 +0700

    AccessHash.pl

--- Hashes/03 Accessing Hash Elements/AccessHash.pl
+++ Hashes/03 Accessing Hash Elements/AccessHash.pl
@@ -1,7 +1,15 @@
 #!/usr/bin/perl
 
 %nama = ('SamirCurcuma' => 65, 'Bombay' => 66, 'Royco' => 67);
+#%vocab = ('Shake' => 69, 'Gummy' => 112, 'Porter' => 116, 'Flamboyant' => 108, 'Mistake' => 109, 'Tapioca' => 79);
 
 print "$nama{'SamirCurcuma'}\n";
 print "$nama{'Bombay'}\n";
 print "$nama{'Royco'}\n";
+
+#print "$vocab{'Shake'}\n";
+#print "$vocab{'Gummy'}\n";
+#print "$vocab{'Porter'}\n";
+#print "$vocab{'Flamboyant'}\n";
+#print "$vocab{'Mistake'}\n";
+#print "$vocab{'Tapioca'}\n";
