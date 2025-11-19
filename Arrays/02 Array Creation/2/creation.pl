b5e7cf70c01a5b5132d50b069627a817e98d600e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 13:49:13 +0700

    Add files via upload

---
+++ Arrays/02 Array Creation/2/creation.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+@array1 = (1, 2, 'Tailor', 'Butter', 'Gazelle', 'Oil');
+@array2 = qw/Ini adalah Sebuah Array/;
+
+print "\$array1[0] = $array1[2]\n";
+print "\$array1[0] = $array1[3]\n";
+print "\$array1[0] = $array1[5]\n";
+print "\$array1[0] = $array1[4]\n";
+
