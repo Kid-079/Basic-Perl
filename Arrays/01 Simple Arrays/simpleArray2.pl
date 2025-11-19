677c94801c028ebaf9ee4e4a7e0ab4ffc06c60e4
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 13:09:03 +0700

    Add files via upload

---
+++ Arrays/01 Simple Arrays/simpleArray2.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+@num = (1, 2, 3, 4, 5);
+@vocab = ("Tailor", "Butter", "Gazelle", "Oil", "Hood");
+
+print "\$num[0] = $num[0]\n";
+print "\$num[1] = $num[1]\n";
+print "\$num[2] = $num[2]\n";
+print "\$num[3] = $num[3]\n";
+print "\$num[4] = $num[4]\n";
+print "\$Text[0] = $vocab[0]\n";
+print "\$Text[1] = $vocab[1]\n";
+print "\$Text[2] = $vocab[2]\n";
+print "\$Text[3] = $vocab[3]\n";
+print "\$Text[4] = $vocab[4]\n";
