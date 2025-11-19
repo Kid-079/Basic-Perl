4192ec0d97956a3aafa355d99d6653e44dd8f299
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 17:07:50 +0700

    Delete Arrays/05 Add and Remove Elements/Add.pl

--- Arrays/05 Add and Remove Elements/Add.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-# Create a simple Array
-@nama = ("hello","Hello","HELLO");
-print "1. \@Nama = @nama\n";
-
-# Add one Element at the end of the array
-@nama = (@nama, "heLLo");
-print "2. \@Nama = @nama\n";
