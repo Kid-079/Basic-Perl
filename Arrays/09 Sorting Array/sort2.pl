11d8849c25076491b4032f9e270fc806525a925b
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 17:18:10 +0700

    Add files via upload

---
+++ Arrays/09 Sorting Array/sort2.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+
+# RUMUS : sort [ SUBROUTINE ] LIST
+
+#define an Array
+@abjad = qw(A B C D E F G);
+print "Abjad : @abjad\n";
+
+#Reset First Index of All Arrays.
+# $[ = 1; # Assigning non-zero to $[ is no longer possible
+
+print "Abjad at \@abjad[1]: $abjad[1]\n";
+print "Abjad at \@abjad[2]: $abjad[2]\n";
