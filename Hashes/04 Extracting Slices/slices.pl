28acc58491c000066afdd6c81c5e8070164519e1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-19 12:20:44 +0700

    slices.pl

--- Hashes/04 Extracting Slices/slices.pl
+++ Hashes/04 Extracting Slices/slices.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl
 
 %nama = (-SamirCurcuma => 65, -Bombay => 66, -Royco => 67);
+#%vocab = ('Shake' => 69, 'Gummy' => 112, 'Porter' => 116, 'Flamboyant' => 108, 'Mistake' => 109, 'Tapioca' => 79);
 
 @array = @nama{-SamirCurcuma, -Royco};
 
+#@array = @vocab{-Gummy, -Mistake, -Tapioca, -Porter};
+
 print "Array : @array\n";
+
+#print "Array : @array\n";
