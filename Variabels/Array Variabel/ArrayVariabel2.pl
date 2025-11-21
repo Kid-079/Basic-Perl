43779578aae365a0fc19c53354751235196cb328
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-21 15:32:05 +0700

    Add files via upload

---
+++ Variabels/Array Variabel/ArrayVariabel2.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+@id = (104, 98, 67, 65, 101);
+@nama = ("Vampire", "Dracula", "Avatar", "Wicked", "Ghost");
+
+print "Id   = $id[1]\n";
+print "Nama = $nama[1]\n";
+
+print "Nama = $nama[4]\n";
+print "Id   = $id[4]\n";
+
+
