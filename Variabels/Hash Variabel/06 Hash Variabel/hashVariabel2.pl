0d53bfdc9e871f9e3fc85f7ee2413df5d0ec9d20
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-21 15:30:02 +0700

    Add files via upload

---
+++ Variabels/Hash Variabel/06 Hash Variabel/hashVariabel2.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+%data = ('Vampire', 104, 'Dracula', 98, 'Avatar', 67, 'Wicked', 65, 'Ghost', 101);
+
+print "Data Avatar = $data{'Avatar'}\n";
+
+print "\$data{'Ghost'} = $data{'Ghost'}\n";
