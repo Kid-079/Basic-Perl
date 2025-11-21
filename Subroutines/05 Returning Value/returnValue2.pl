94894772949efcf6908f7b3908bd7146992b5c51
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-21 14:04:29 +0700

    Add files via upload

---
+++ Subroutines/05 Returning Value/returnValue2.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# Function Definition 
+sub Average {
+	# get Total number of arguments passed
+	$n = scalar(@_);
+	$sum = 0;
+
+	foreach $item (@_) {
+		$sum += $item;
+	}
+	$average = $sum / $n;
+
+	return $average;
+}
+
+# Function Call
+Average(10,20,30,40,50,60,70);
+print "Average for the given numbers : $average\n";
