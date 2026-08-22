4d4af987d150f744d2b0982b607b3e32b60818e6
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 20:09:47 +0700

    Update vstring.pl

--- Scalars/06 V-String/vstring.pl
+++ Scalars/06 V-String/vstring.pl
@@ -4,11 +4,11 @@ $emoticon1 = v128530;
 $emoticon2 = v128568;
 $emoticon3 = v128530.32.128568.32.128569;
 
-$text = v72.101.108.108.111;
-$name = v66.114.111.116.104.101.114;
+$text1 = v60.78.33.103.72.116;
+$text2 = v114.51.36.80.101.24.84;
 
 print "emoticon = $emoticon1\n";
 print "emoticon = $emoticon2\n";
 print "emoticon = $emoticon3\n";
-print "Teks = $text\n";
-print "Nama = $name\n";
+print "Text1 = $text1\n";
+print "Text2 = $text2\n";
