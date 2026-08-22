cae697860d74a0654643a0530d16c04710f3e304
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 17:15:23 +0700

    Update simple.pl

--- Hashes/01 Simple Hashes/simple.pl
+++ Hashes/01 Simple Hashes/simple.pl
@@ -3,11 +3,11 @@
 %kata = ('Knight', 38, 'Tinkle', 69, 'Coboy', 52, 'Falcon', 83, 'Spark', 43 );
 #%kata = ('Ornament', 101, 'Lonely', 89, 'Wise', 104,'Grizzly',  111, 'Shoe', 78);
 
-print "\$kata{'Hello'} = $kata{'Hello'}\n";
-print "\$kata{'HELLO'} = $kata{'HELLO'}\n";
-print "\$kata{'hello'} = $kata{'hello'}\n";
-print "\$kata{'heLLo'} = $kata{'heLLo'}\n";
-print "\$kata{'HellO'} = $kata{'HellO'}\n";
+print "\$kata{'Knight'} = $kata{'Knight'}\n";
+print "\$kata{'Tinkle'} = $kata{'Tinkle'}\n";
+print "\$kata{'Coboy'} = $kata{'Coboy'}\n";
+print "\$kata{'Falcon'} = $kata{'Falcon'}\n";
+print "\$kata{'Spark'} = $kata{'Spark'}\n";
 
 #print "\$kata{'Ornament'} = $kata{'Ornament'}\n";
 #print "\$kata{'Lonely'} = $kata{'Lonely'}\n";
