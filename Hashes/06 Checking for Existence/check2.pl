c8d2f5aa0ea16b235bac4f37a80969f39d7442f1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 18:40:44 +0700

    Update check2.pl

--- Hashes/06 Checking for Existence/check2.pl
+++ Hashes/06 Checking for Existence/check2.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl
 
-%data = ('A' => 54, 'B' => 55, 'C' => 49, 'D' => 50, 'E' => 51, 'F' => 53);
+%data = ('Rumble' => 67, 'Mighty' => 48, 'Thunder' => 98, 'Meteor' => 79, 'Respect' => 89);
+#%data = ('Knight' => 52, 'Tinkle' => 83, 'Coboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
+#%data = ('A' => 54, 'B' => 55, 'C' => 49, 'D' => 50, 'E' => 51, 'F' => 53);
 #%data = ('A' => 53, 'B' => 49, 'C' => 50, 'D' => 51, 'E' => 49, 'F' => 50, 'G' => 51, 'H' => 50);
 
-if ( exists($data{'G'}) ) {
-	print "Data D memiliki nilai : $data{'C'}\n";
+if ( exists($data{'Respect'}) ) {
+	print "Data Respect : $data{'Respect'}\n";
 } else {
 	print "Check Variabel.!!\n";
 }
