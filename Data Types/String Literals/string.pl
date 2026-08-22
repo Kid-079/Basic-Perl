615da3c3ef9dc86a95705b559993f3c6a6ccf04d
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 16:56:38 +0700

    Update string.pl

--- Data Types/String Literals/string.pl
+++ Data Types/String Literals/string.pl
@@ -1,23 +1,23 @@
 #!/usr/bin/perl
 
 #This is case of interpolation (Menyisipkan atau Menambahkan)
-$string = "Hello world\n";
+$string = "Voices Whisper In The Trees, Shadows Of A Thousand Years Rise Again.\n";
 print "$string\n";
 
 # This is case of non-interpolation (Menyisipkan atau Menambahkan)
-$string= 'Hello \nWorld';
+$string= 'The light has been broken, \nThe moon has awoken with the sleep of the sun';
 print "$string\n";
 
 # Membuat huruf besar pada satu huruf saja
-$string= "\uhello \uhello";
+$string= "\uthere is magic in the night \uwhen pumpkins glow by moonlight";
 print "$string\n";
 
 # Membuat semua huruf menjadi huruf besar
-$string= "\Uhello hello";
+$string= "\UThe light has been broken, The moon has awoken with the sleep of the sun";
 print "$string\n";
 
 # Membuat huruf besar pada kalimat yang diinginkan
-$string= "hello \Uwelcome\E hello";
+$string= "there is \Umagic\E in the \Unight\E \uwhen pumpkins \Uglow\E by \Umoonlight\E";
 print "$string\n";
 
 # Backslash non alpha-numeric including spaces
