eb9d331f44bb02236822f7ec9e3cc6b5ed3590f7
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 00:06:27 +0700

    Update ArrayVariabel.pl

--- Variabels/Array Variabel/ArrayVariabel.pl
+++ Variabels/Array Variabel/ArrayVariabel.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/perl
 
-@umur = (78, 90, 67, 73);
-@nama = ("Kumar", "Kajol", "Sambal", "Pedas");
+@id = (52, 83, 116, 27, 50, 33, 90, 60)
+@vocab ("Knight", "Tinkle", "Coboy", "Falcon", "Spark", "Beast", "Fairy", "Baboon")
+#@umur = (78, 90, 67, 73);
+#@nama = ("Kumar", "Kajol", "Sambal", "Pedas");
 
-print "Umur = $umur[2]\n";
-print "Nama = $nama[2]\n";
+print "ID    = $id[5]\n";
+print "Vocab = $vocab[5]\n";
+#print "Umur = $umur[2]\n";
+#print "Nama = $nama[2]\n";
 
-print "Nama = $nama[3]\n";
-print "Umur = $umur[3]\n";
+print "ID    = $id[2]\n";
+print "Vocab = $vocab[2]\n";
+#print "Nama = $nama[3]\n";
+#print "Umur = $umur[3]\n";
 
