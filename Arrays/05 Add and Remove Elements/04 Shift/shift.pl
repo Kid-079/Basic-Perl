654136b28e202b5a3b9c2faa86ef8258ea245827
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 16:02:25 +0700

    Update shift.pl

--- Arrays/05 Add and Remove Elements/04 Shift/shift.pl
+++ Arrays/05 Add and Remove Elements/04 Shift/shift.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl
 
 # Create a simple Array
-@nama = ("hello","Hello","HELLO");
+@nama = ("falcon","Garden","KNIGHT","Spark","Fairy");
 print "1. \@Nama = @nama\n";
 
 
 # Add one Element at the end of the array
-unshift(@nama, "heLLo");
+unshift(@nama, "Coboy");
 print "2. \@Nama = @nama\n";
 
 # Remove One Element From the beginning of the array
