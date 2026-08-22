4d2c32d0cb82e5f049031997b03799f0f8dd33b6
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 19:12:34 +0700

    Update hello.pl

--- Intro/hello.pl
+++ Intro/hello.pl
@@ -3,6 +3,6 @@
 # Ini adalah Comment
 print "There Is Magic In The Night, When Pumpkins Glow By Moonlight\n";
 
-print ("Hello World\n");
+print ("Voices Whisper In The Trees, Shadows Of A Thousand Years Rise Again.\n");
 
 print 'Hello\n';
