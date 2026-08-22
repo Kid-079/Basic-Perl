771580b3cdcfc4497530f73a94b139d6e2599f80
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 17:45:07 +0700

    Update keys.pl

--- Hashes/05 Extracting Key and Values/1/keys.pl
+++ Hashes/05 Extracting Key and Values/1/keys.pl
@@ -2,15 +2,25 @@
 
 # RUMUS : keys %HASH
 
-%data = ('Paula Godzila' => 79, 'Margarita' => 86, 'Tina Ketumbar' => 74);
+%data = (Knight => 52, Tinkle => 83, Coboy => 116, Falcon => 27, Spark => 50, Beast => 33, Fairy => 90, Baboon => 60 )
+#%data = ('Paula Godzila' => 79, 'Margarita' => 86, 'Tina Ketumbar' => 74);
 #%data = ('Vampire' => 121, 'Dracula' => 122, 'Avatar' => 74, 'Wicked' => 90, 'Ghost' => 65);
 
-@nama = keys %data;
+@name = keys %data;
 #@vocab = keys %data;
 
-print "$nama[0]\n";
-print "$nama[1]\n";
-print "$nama[2]\n";
+print "$vocab[0]\n";
+print "$vocab[1]\n";
+print "$vocab[2]\n";
+print "$vocab[3]\n";
+print "$vocab[4]\n";
+print "$vocab[5]\n";
+print "$vocab[6]\n";
+print "$vocab[7]\n";
+
+#print "$name[0]\n";
+#print "$name[1]\n";
+#print "$name[2]\n";
 
 #print "$vocab[0]\n";
 #print "$vocab[1]\n";
