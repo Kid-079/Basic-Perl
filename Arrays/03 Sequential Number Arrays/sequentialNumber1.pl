0d7c3034e434916de314501a81a7974ca5672b7d
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 14:55:03 +0700

    Add files via upload

---
+++ Arrays/03 Sequential Number Arrays/sequentialNumber1.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+@var_8 = (1..8);
+@var_15 = (10..25);
+@var_AZL = (a..z);
+@var_AZH = (A..Z);
+
+print "@var_8\n";		# Print angka dari 1 - 8
+print "@var_15\n";		# Print angka dari 10 - 25
+print "@var_AZL\n";		# Print huruf dari a - z
+print "@var_AZH\n";		# Print huruf dari A - Z
---
+++ Arrays/03 Sequential Number Arrays/sequentialNumber2.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+@var_10 = (1..10);
+@var_30 = (1..30);
+@var_50 = (1..50);
+@var_AZL = (a..z);
+@var_AZH = (A..Z);
+
+print "@var_10\n";		# Print angka dari 1 - 10
+print "@var_30\n";		# Print angka dari 1 - 30
+print "@var_50\n";              # Print angka dari 1 - 50
+print "@var_AZL\n";		# Print huruf dari a - z
+print "@var_AZH\n";		# Print huruf dari A - Z
