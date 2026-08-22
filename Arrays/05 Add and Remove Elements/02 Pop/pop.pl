388da65907894b524696425859a0bf0fa066abd3
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 15:37:27 +0700

    Update pop.pl

--- Arrays/05 Add and Remove Elements/02 Pop/pop.pl
+++ Arrays/05 Add and Remove Elements/02 Pop/pop.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl
 
 # Create a simple Array
-@nama = ("hello","Hello","HELLO");
+@nama = ("Spark","Fairy","FALCON","garden","Knight");
 print "1. \@Nama = @nama\n";
 
 
 # Add one Element at the end of the array
-pop(@nama, "heLLo");
+pop(@nama, "COBOY");
 print "2. \@Nama = @nama\n";
 
 # Remove One Element From the last of the array
