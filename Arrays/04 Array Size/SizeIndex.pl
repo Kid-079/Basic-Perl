f229758bc47ad5ef080a5851c1511d74e0243133
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 15:18:46 +0700

    Update SizeIndex.pl

--- Arrays/04 Array Size/SizeIndex.pl
+++ Arrays/04 Array Size/SizeIndex.pl
@@ -1,11 +1,9 @@
 #!/usr/bin/perl
 
-@array = (1,2,3,4,5,6,7); 
-
+@array = (1,2,3,4,5,6,7,8,9,10); 
 
 $size = @array;
 $max_index = $#array;
-
 
 print "Size      : $size\n";
 print "Max Index : $max_index\n";
