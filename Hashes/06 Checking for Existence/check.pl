99b7752d44f2618c965741f3b16cf365886507b1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 18:29:46 +0700

    Update check.pl

--- Hashes/06 Checking for Existence/check.pl
+++ Hashes/06 Checking for Existence/check.pl
@@ -3,7 +3,7 @@
 %data = ('Rumble' => 67, 'Mighty' => 48, 'Thunder' => 98, 'Meteor' => 79, 'Respect' => 89);
 #%data = ('Knight' => 52, 'Tinkle' => 83, 'Coboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
 
-if ( exists($data{'D'}) ) {
+if ( exists($data{'Thunder'}) ) {
 	print "Data Thunder : $data{'Thunder'}\n";
 	#print "Data Respect : $data{'Respect'}\n";
 } else {
