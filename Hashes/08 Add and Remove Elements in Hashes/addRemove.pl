38cb3b13d2a8ef1655616b12b94037c3cb732884
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 18:59:37 +0700

    Update addRemove.pl

--- Hashes/08 Add and Remove Elements in Hashes/addRemove.pl
+++ Hashes/08 Add and Remove Elements in Hashes/addRemove.pl
@@ -1,20 +1,24 @@
 #!/usr/bin/perl
 
-%data = ('A' => 50, 'B' => 55, 'C' => 48, 'D' => 67);
+%data = ('Rumble' => 67, 'Mighty' => 48, 'Thunder' => 98, 'Meteor' => 79, 'Respect' => 89);
+#%data = ('Knight' => 52, 'Tinkle' => 83, 'Coboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
+#%data = ('A' => 50, 'B' => 55, 'C' => 48, 'D' => 67);
+
 @keys = keys %data;
 $size = @keys;
 print "1 - Hash Size: is $size\n";
 
-
 # Adding an Element to the Hash;
-$data{'E'} = 72;
+$data{'Phantom'} = 33;
+#$data{'E'} = 72;
 @keys = keys %data;
 $size = @keys;
 print "2 - Hash Size: is $size\n";
 
 
 # Delete The Same Element from the Hash
-delete $data{'E'};
+delete $data{'Phantom'};
+#delete $data{'E'};
 @keys = keys %data;
 $size = @keys;
 print "3 - Hash Size: is $size\n";
