4b30846fe5858aa30e988a1041342aa5523574cf
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 19:37:19 +0700

    Add files via upload

---
+++ Intro/introduction1.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+# This is Comment
+print "There Is Magic In The Night, When Pumpkins Glow By Moonlight\n";
+
+print ("Voices Whisper In The Trees, Shadows Of A Thousand Years Rise Again.\n");
+
+print 'The Light Has Been Broken, The Moon Has Awoken With The Sleep Of The Sun\n';
