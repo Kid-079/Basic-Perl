7231c5d9092898844c2f2f99122edd30f6113b08
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 14:26:46 +0700

    Update creation.pl

--- Arrays/02 Array Creation/1/creation.pl
+++ Arrays/02 Array Creation/1/creation.pl
@@ -1,20 +1,27 @@
 #!/usr/bin/perl
 
-@array1 = (1, 2, 'Kumar', 'Salaman', 'Anjeli', 'Damai');
-#@array2 = (1, 2, 3, 4, 5, 'Vampire', 'Dracule', 'Wicked', 'Ghost');
-@array3 = qw/There Is Magic In The Night, When Pumpkins Glow By Moonlight/;
+@array1 = (1, 2, 3, 4, 5, 'Rumble', 'Tinker', 'Asterisk', 'Baboon', 'Beast');
+#@array2 = (1, 2, 'Kumar', 'Salaman', 'Anjeli', 'Damai');
+#@array3 = (1, 2, 3, 4, 5, 'Vampire', 'Dracule', 'Wicked', 'Ghost');
+@array4 = qw/There Is Magic In The Night, When Pumpkins Glow By Moonlight/;
 
-print "\$array1[0] = $array1[2]\n";
-print "\$array1[0] = $array1[4]\n";
-print "\$array1[0] = $array1[3]\n";
-print "\$array1[0] = $array1[5]\n";
 
-#print "\$array2[0] = $array2[3]\n";
-#print "\$array2[0] = $array2[8]\n";
-#print "\$array2[0] = $array2[1]\n";
-#print "\$array2[0] = $array2[5]\n";
-#print "\$array2[0] = $array2[4]\n";
-#print "\$array2[0] = $array2[7]\n";
 #print "\$array2[0] = $array2[2]\n";
-#print "\$array2[0] = $array2[6]\n";
+#print "\$array2[0] = $array2[4]\n";
+#print "\$array2[0] = $array2[3]\n";
+#print "\$array2[0] = $array2[5]\n";
+
+#print "\$array2[0] = $array2[2]\n";
+#print "\$array2[0] = $array2[4]\n";
+#print "\$array2[0] = $array2[3]\n";
+#print "\$array2[0] = $array2[5]\n";
+
+#print "\$array3[0] = $array3[3]\n";
+#print "\$array3[0] = $array3[8]\n";
+#print "\$array3[0] = $array3[1]\n";
+#print "\$array3[0] = $array3[5]\n";
+#print "\$array3[0] = $array3[4]\n";
+#print "\$array3[0] = $array3[7]\n";
+#print "\$array3[0] = $array3[2]\n";
+#print "\$array3[0] = $array3[6]\n";
 
