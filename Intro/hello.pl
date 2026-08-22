0f1b5c6de7d45f1e8f1e8a29419fc8368df76494
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 19:13:29 +0700

    Update hello.pl

--- Intro/hello.pl
+++ Intro/hello.pl
@@ -5,4 +5,4 @@ print "There Is Magic In The Night, When Pumpkins Glow By Moonlight\n";
 
 print ("Voices Whisper In The Trees, Shadows Of A Thousand Years Rise Again.\n");
 
-print 'Hello\n';
+print 'The Light Has Been Broken, The Moon Has Awoken With The Sleep Of The Sun\n';
