3bf5766e79f6878c8808ed84dd3ae8f8b51ac2fa
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 14:23:26 +0700

    Update creation.pl

--- Arrays/02 Array Creation/2/creation.pl
+++ Arrays/02 Array Creation/2/creation.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 @array1 = (1, 2, 'Tailor', 'Butter', 'Gazelle', 'Oil');
-@array2 = qw/Ini adalah Sebuah Array/;
+@array2 = qw/There Is Magic In The Night, When Pumpkins Glow By Moonlight/;
 
 print "\$array1[0] = $array1[2]\n";
 print "\$array1[0] = $array1[3]\n";
