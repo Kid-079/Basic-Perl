7de5325fa8f384456c16e148738d4f0a07f2ab4c
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 19:52:05 +0700

    Update operations.pl

--- Scalars/04 Operations Scalar/operations.pl
+++ Scalars/04 Operations Scalar/operations.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 
 $string = "There Is Magic In The Night, " . "When Pumpkins Glow By Moonlight";	# Concatenates String
-$num = 9 + 16;					# Adds 2 numbers
-$mul = 4 * 5;					# Multiplies 2 numbers
+$num = 9 + 16;					      # Adds 2 numbers
+$mul = 4 * 5;					        # Multiplies 2 numbers
 $mix = $string . $num;				# Concatenates String and number
 
 
