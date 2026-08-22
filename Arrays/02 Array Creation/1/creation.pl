aeef70876b3707ee43a670c830ecf8b9482b11b9
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 14:46:37 +0700

    Update creation.pl

--- Arrays/02 Array Creation/1/creation.pl
+++ Arrays/02 Array Creation/1/creation.pl
@@ -6,10 +6,11 @@
 @array4 = qw/There Is Magic In The Night, When Pumpkins Glow By Moonlight/;
 
 
-print "\$array1[0] = $array1[2]\n";
+print "\$array1[0] = $array1[0]\n";
+print "\$array1[0] = $array1[8]\n";
+print "\$array1[0] = $array1[6]\n";
 print "\$array1[0] = $array1[4]\n";
-print "\$array1[0] = $array1[3]\n";
-print "\$array1[0] = $array1[5]\n";
+print "\$array1[0] = $array1[2]\n";
 
 #print "\$array2[0] = $array2[2]\n";
 #print "\$array2[0] = $array2[4]\n";
