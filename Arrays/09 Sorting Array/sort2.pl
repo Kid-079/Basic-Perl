74b8ac601a8373b003853c698058faf5f41ddc3b
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 16:36:03 +0700

    Update sort2.pl

--- Arrays/09 Sorting Array/sort2.pl
+++ Arrays/09 Sorting Array/sort2.pl
@@ -4,11 +4,11 @@
 # RUMUS : sort [ SUBROUTINE ] LIST
 
 #define an Array
-@abjad = qw(A B C D E F G);
+@abjad = qw(A B C D E F G H I J);
 print "Abjad : @abjad\n";
 
 #Reset First Index of All Arrays.
 # $[ = 1; # Assigning non-zero to $[ is no longer possible
 
-print "Abjad at \@abjad[1]: $abjad[1]\n";
 print "Abjad at \@abjad[2]: $abjad[2]\n";
+print "Abjad at \@abjad[9]: $abjad[9]\n";
