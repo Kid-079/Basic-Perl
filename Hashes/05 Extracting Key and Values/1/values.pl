ac6804e33c0806e9e049ba932a976b3565ee2814
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 17:48:37 +0700

    Update values.pl

--- Hashes/05 Extracting Key and Values/1/values.pl
+++ Hashes/05 Extracting Key and Values/1/values.pl
@@ -2,7 +2,8 @@
 
 # RUMUS : values %HASH
 
-%data = ('Paula Godzila' => 79, 'Margarita' => 86, 'Tina Ketumbar' => 74);
+%data = ('Knight' => 52, 'Tinkle' => 83, 'Coboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60)
+#%data = ('Paula Godzila' => 79, 'Margarita' => 86, 'Tina Ketumbar' => 74);
 #%data = ('Vampire' => 121, 'Dracula' => 122, 'Avatar' => 74, 'Wicked' => 90, 'Ghost' => 65);
 
 @nilai = values %data;
@@ -10,6 +11,15 @@
 print "$nilai[0]\n";
 print "$nilai[1]\n";
 print "$nilai[2]\n";
+print "$nilai[3]\n";
+print "$nilai[4]\n";
+print "$nilai[5]\n";
+print "$nilai[6]\n";
+print "$nilai[7]\n";
+
+#print "$nilai[0]\n";
+#print "$nilai[1]\n";
+#print "$nilai[2]\n";
 
 #print "$nilai[0]\n";
 #print "$nilai[1]\n";
