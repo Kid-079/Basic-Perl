cbb6653c4674028545d416e2d10b51aae203a6ac
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 17:33:33 +0700

    Update slices.pl

--- Hashes/04 Extracting Slices/slices.pl
+++ Hashes/04 Extracting Slices/slices.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl
 
-%nama = (-SamirCurcuma => 65, -Bombay => 66, -Royco => 67);
+%vocab = (-Knight => 52, -Tinkle => 83, Coboy => 116, Falcon => 27, Spark => 50, Beast => 33, Fairy => 90, Baboon => 60 )
+#%name = (-SamirCurcuma => 65, -Bombay => 66, -Royco => 67);
 #%vocab = ('Shake' => 69, 'Gummy' => 112, 'Porter' => 116, 'Flamboyant' => 108, 'Mistake' => 109, 'Tapioca' => 79);
 
-@array = @nama{-SamirCurcuma, -Royco};
-
+@array = @vocab{-Tinkle, -Knight, -Fairy, -Spark};
+#@array = @name{-SamirCurcuma, -Royco};
 #@array = @vocab{-Gummy, -Mistake, -Tapioca, -Porter};
 
 print "Array : @array\n";
