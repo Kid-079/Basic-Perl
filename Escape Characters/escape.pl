65947404a2539ba933f43de87cac083b8de24f17
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 17:01:41 +0700

    Update escape.pl

--- Escape Characters/escape.pl
+++ Escape Characters/escape.pl
@@ -1,11 +1,9 @@
 #!/usr/bin/perl
 
-$result = "Ini adalah sebuah Angka\n";
-
+$result = "The Light Has Been Broken, The Moon Has Awoken With The Sleep Of The Sun.\n";
 
 # Mencetak isi dari variabel
 print "$result\n";
-
 
 # Mencetak variabelnya
 #print "\$result\n"
