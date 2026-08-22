9f59fe55867f833dd8d29af5d15a2ab0a81f9dc1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 15:20:28 +0700

    Update SizeIndex2.pl

--- Arrays/04 Array Size/SizeIndex2.pl
+++ Arrays/04 Array Size/SizeIndex2.pl
@@ -3,15 +3,12 @@
 @array1 = (1,2,3,4,5,6,7,8,9,10); 
 @array2 = ('A','B','C','D','E','F','G','H','I','J'); 
 
-
 # I
 $size = @array1;
 $max_index = $#array1;
 
 print "Size      : $size\n";
 print "Max Index : $max_index\n";
-
-
 
 print "showArray [2] = $array1[2]\n";
 print "showArray [-5] = $array2[-5]\n";
