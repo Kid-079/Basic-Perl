6fdf71015d1b9ac7e45163a7242bd8075978aece
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-22 19:37:36 +0700

    Delete Intro/hello.pl

--- Intro/hello.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-# Ini adalah Comment
-print "There Is Magic In The Night, When Pumpkins Glow By Moonlight\n";
-
-print ("Voices Whisper In The Trees, Shadows Of A Thousand Years Rise Again.\n");
-
-print 'The Light Has Been Broken, The Moon Has Awoken With The Sleep Of The Sun\n';
