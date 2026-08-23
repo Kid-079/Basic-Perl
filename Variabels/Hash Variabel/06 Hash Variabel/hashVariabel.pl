9bca2091d645916b206f81b0c4d34d508f253c23
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 10:55:17 +0700

    Update hashVariabel.pl

--- Variabels/Hash Variabel/06 Hash Variabel/hashVariabel.pl
+++ Variabels/Hash Variabel/06 Hash Variabel/hashVariabel.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-%data = ('Knight', 52, 'Tinkle', 83, 'Coboy', 116, 'Falcon', 27, 'Spark', 50, 'Beast', 33, 'Fairy', 90, 'Baboon', 60)
+%data = ('Knight', 52, 'Tinkle', 83, 'Coboy', 116, 'Falcon', 27, 'Spark', 50, 'Beast', 33, 'Fairy', 90, 'Baboon', 60);
 #%data = ('Kumar', 32, 'Sambal', 67, 'Pedas', 84);
 
 print "Data Coboy = $data{'Coboy'}\n";
