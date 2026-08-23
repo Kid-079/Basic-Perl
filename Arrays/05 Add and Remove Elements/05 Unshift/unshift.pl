c8c60b3e0b3855602313f7d7a82f360ab9ed4c87
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 13:30:13 +0700

    Update unshift.pl

--- Arrays/05 Add and Remove Elements/05 Unshift/unshift.pl
+++ Arrays/05 Add and Remove Elements/05 Unshift/unshift.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # Create a simple Array
-@nama = ("GARDEN","fairy","Spark","knight","Coboy");
+@nama = ("GARDEN","fairy","Spark","knight","Cowboy");
 print "1. \@Nama = @nama\n";
 
 # Add one Element at the beginning of the array
