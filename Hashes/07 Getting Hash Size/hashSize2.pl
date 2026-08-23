11809cb8f7b8e253c755f30c627627a9e171ae99
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 13:34:09 +0700

    Update hashSize2.pl

--- Hashes/07 Getting Hash Size/hashSize2.pl
+++ Hashes/07 Getting Hash Size/hashSize2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 %data = ('Rumble' => 67, 'Mighty' => 48, 'Thunder' => 98, 'Meteor' => 79, 'Respect' => 89);
-#%data = ('Knight' => 52, 'Tinkle' => 83, 'Coboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
+#%data = ('Knight' => 52, 'Tinkle' => 83, 'Cowboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
 #%data = ('A' => 54, 'B' => 55, 'C' => 49, 'D' => 50, 'E' => 51, 'F' => 53, 'G' => 52);
 
 @keys = keys %data;
