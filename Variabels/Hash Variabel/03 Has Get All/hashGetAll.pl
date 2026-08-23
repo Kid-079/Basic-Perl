bd8f4901d5d5923c5bb05f7d398cc3876157b956
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 14:06:53 +0700

    Update hashGetAll.pl

--- Variabels/Hash Variabel/03 Has Get All/hashGetAll.pl
+++ Variabels/Hash Variabel/03 Has Get All/hashGetAll.pl
@@ -3,23 +3,35 @@
 use warnings;
 use strict;
 
-# defines country => language hash
+#1 - defines country => language hash
+#2 - defines vocabulary => vocabulary hash
 
-my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
+my %vocab = (Knight => 'Never Give Up', Tinkle => 'The Beauty Of Life', Beast => 'We Are Warrior', Falcon => 'Stronger Than The Storm', Cowboy => 'Talk Slowly, Think Quickly');
+#my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
 
-# get language 
-print "$languages{'England'}\n";
-print "$languages{'France'}\n";
-print "$languages{'Spain'}\n";
-print "$languages{'China'}\n";
+# get data
+print "$vocab{'Tinkle'}\n";
+print "$vocab{'Falcon'}\n";
+print "$vocab{'Beast'}\n";
+print "$vocab{'Knight'}\n";
 print "\n";
+#print "$languages{'England'}\n";
+#print "$languages{'France'}\n";
+#print "$languages{'Spain'}\n";
+#print "$languages{'China'}\n";
+#print "\n";
 
 # delete language
-delete $languages{'China'};
+delete $vocab{'Cowboy'};
+#delete $languages{'China'};
 
 # get language 
-print "$languages{'England'}\n";
-print "$languages{'France'}\n";
-print "$languages{'Spain'}\n";
-print "$languages{'China'}\n"; # Use of uninitialized value (Delete Success)
+print "$vocab{'Tinkle'}\n";
+print "$vocab{'Falcon'}\n";
+print "$vocab{'Beast'}\n";
+print "$vocab{'Knight'}\n"; # Use of uninitialized value (Delete Success)
+#print "$languages{'England'}\n";
+#print "$languages{'France'}\n";
+#print "$languages{'Spain'}\n";
+#print "$languages{'China'}\n"; # Use of uninitialized value (Delete Success)
 
