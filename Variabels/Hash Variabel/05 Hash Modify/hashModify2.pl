65d2d9c0f0a6b5c0bb52fa6924f0de549fea2984
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 14:43:44 +0700

    Update hashModify2.pl

--- Variabels/Hash Variabel/05 Hash Modify/hashModify2.pl
+++ Variabels/Hash Variabel/05 Hash Modify/hashModify2.pl
@@ -9,31 +9,46 @@ use strict;
 my %vocab = (Knight => 'Never Give Up', Tinkle => 'The Beauty Of Life', Beast => 'We Are Warrior', Falcon => 'Stronger Than The Storm', Cowboy => 'Talk Slowly, Think Quickly');
 #my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
 
-# get data 
+# get data
 my $language1 = $languages{'England'};
 my $language2 = $languages{'France'};
 my $language3 = $languages{'Spain'};
 my $language4 = $languages{'China'};
+#my $language1 = $languages{'England'};
+#my $language2 = $languages{'France'};
+#my $language3 = $languages{'Spain'};
+#my $language4 = $languages{'China'};
 
 # print data
 print($language1, "\n");
 print($language2, "\n");
 print($language3, "\n");
 print($language4, "\n");
+#print($language1, "\n");
+#print($language2, "\n");
+#print($language3, "\n");
+#print($language4, "\n");
 
 # Add New data
 $languages{'Germany'} = 'Jerman';
 $languages{'India'} = 'Pegal he.. Pegal he...';
+#$languages{'Germany'} = 'Jerman';
+#$languages{'India'} = 'Pegal he.. Pegal he...';
 
 # Modify data
 $languages{'India'} = 'Hindustan';
+#$languages{'India'} = 'Hindustan';
 
 # get new data
 my $language5 = $languages{'Germany'};
 my $language6 = $languages{'India'};
+#my $language5 = $languages{'Germany'};
+#my $language6 = $languages{'India'};
 
 # print New data
 print($language5, "\n");
 print($language6, "\n");
+#print($language5, "\n");
+#print($language6, "\n");
 
 
