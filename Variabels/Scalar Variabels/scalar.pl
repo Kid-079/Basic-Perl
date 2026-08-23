59125f5211a8316ae03edfc2a24b0e8a59e08c80
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 11:04:14 +0700

    Update scalar.pl

--- Variabels/Scalar Variabels/scalar.pl
+++ Variabels/Scalar Variabels/scalar.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
 $data = "Beast";
-$id = ;
+$id = 75110335710484;
 #$nama = "Kumar Salaman";
 #$umur = 90;
 
+print "Data = $data\n";
+print "ID = $id\n";
 #print "Nama = $nama\n";
 #print "Umur = $umur\n";
