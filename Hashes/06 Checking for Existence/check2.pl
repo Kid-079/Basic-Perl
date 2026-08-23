cb703f6f6950e552ca39ed87e87b8e2de159874b
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 13:33:44 +0700

    Update check2.pl

--- Hashes/06 Checking for Existence/check2.pl
+++ Hashes/06 Checking for Existence/check2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 %data = ('Rumble' => 67, 'Mighty' => 48, 'Thunder' => 98, 'Meteor' => 79, 'Respect' => 89);
-#%data = ('Knight' => 52, 'Tinkle' => 83, 'Coboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
+#%data = ('Knight' => 52, 'Tinkle' => 83, 'Cowboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
 #%data = ('A' => 54, 'B' => 55, 'C' => 49, 'D' => 50, 'E' => 51, 'F' => 53);
 #%data = ('A' => 53, 'B' => 49, 'C' => 50, 'D' => 51, 'E' => 49, 'F' => 50, 'G' => 51, 'H' => 50);
 
