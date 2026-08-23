562258d8f79ed09cbc2522ac9b4fb525cbfddecc
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 14:01:14 +0700

    Update hashGet2.pl

--- Variabels/Hash Variabel/02 Hash Get/hashGet2.pl
+++ Variabels/Hash Variabel/02 Hash Get/hashGet2.pl
@@ -3,14 +3,20 @@
 use warnings;
 use strict;
 
-# defines country => language hash
+#1 - defines country => language hash
+#2 - defines vocabulary => vocabulary hash
 
-my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
+my %vocab = (Knight => 'Never Give Up', Tinkle => 'The Beauty Of Life', Beast => 'We Are Warrior', Falcon => 'Stronger Than The Storm', Cowboy => 'Talk Slowly, Think Quickly');
+#my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
 
-# get language 
-my $language = $languages{'England'};
-my $language2 = $languages{'France'};
+# get data
+my $vocab1 = $vocab{'Cowboy'};
+my $vocab2 = $vocab{'Knight'};
+#my $language = $languages{'England'};
+#my $language2 = $languages{'France'};
 
 # print language
-print($language, "\n");
-print($language2, "\n");
+print($vocab1, "\n");
+print($vocab2, "\n");
+#print($language, "\n");
+#print($language2, "\n");
