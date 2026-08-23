3d7d33dbbd2602b70058484c00dfe30fa5ff889e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 13:56:34 +0700

    Update hashGet.pl

--- Variabels/Hash Variabel/02 Hash Get/hashGet.pl
+++ Variabels/Hash Variabel/02 Hash Get/hashGet.pl
@@ -6,7 +6,7 @@ use strict;
 #1 - defines country => language hash
 #2 - defines vocabulary => vocabulary hash
 
-my %vocab = (Knight => 'Never Give Up', Tinkle => 'The Beauty Of Life', Beast => 'We Are Warrior', Falcon => 'Stronger Than The Storm');
+my %vocab = (Knight => 'Never Give Up', Tinkle => 'The Beauty Of Life', Beast => 'We Are Warrior', Falcon => 'Stronger Than The Storm', Cowboy => 'Talk Slowly, Think Quickly');
 #my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
 
 # get data 
