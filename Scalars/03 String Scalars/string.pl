8fa9daa9ce0b393cb7265f0841bac18db1c5c625
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 11:30:43 +0700

    Update string.pl

--- Scalars/03 String Scalars/string.pl
+++ Scalars/03 String Scalars/string.pl
@@ -4,7 +4,7 @@ $var = "Ini adalah String Scalar";
 $quote = 'Ini ditulis dengan menggunakan kutip satu - $var';
 $double = "Ini ditulis dengan menggunakan kutip dua - $var";
 
-$escape = "Ini adalah contoh penggunaan escape character TAB - \tHello Perl";
+$escape = "Ini adalah contoh penggunaan escape character TAB - \tThere Is Magic In The Night, When Pumpkins Glow By Moonlight";
 
 print "var = $var\n";
 print "Quote = $quote\n";
