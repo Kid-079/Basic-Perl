3ff2cd871b8658dab973ef98a81a72b1b3b767b8
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 14:19:00 +0700

    Update hashModify.pl

--- Variabels/Hash Variabel/05 Hash Modify/hashModify.pl
+++ Variabels/Hash Variabel/05 Hash Modify/hashModify.pl
@@ -3,26 +3,30 @@
 use warnings;
 use strict;
 
-# defines country => language hash
+#1 - defines country => language hash
+#2 - defines vocabulary => vocabulary hash
 
-my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
+my %vocab = (Knight => 'Never Give Up', Tinkle => 'The Beauty Of Life', Falcon => 'Stronger Than The Storm', Cowboy => 'Talk Slowly, Think Quickly');
+#my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
 
-# Add language
-$languages{'India'} = 'Aca Aca';
+# Add data
+$vocab{'Beast'} = 'We Are Warrior'; 
+#$languages{'India'} = 'Aca Aca';
 
-# Print language 
+# Print Data
 print "Before Modify :\n";
-print "$languages{'England'}\n";
-print "$languages{'France'}\n";
-print "$languages{'Spain'}\n";
-print "$languages{'China'}\n";
-print "$languages{'India'}\n";
+print "$vocab{'Knight'}\n";
+print "$vocab{'Tinkle'}\n";
+print "$vocab{'Falcon'}\n";
+print "$vocab{'Cowboy'}\n";
+print "$vocab{'Beast'}\n";
 print "\n";
 
-# Modify Language
-$languages{'India'} = 'Hindustan';
+# Modify Data
+$vocab{'Beast'} = 'Stay Strong And Keep Fighting';
+#$vocab{'Beast'} = 'Stay Strong And Keep Fighting';
 
-# Print language 
+# Print Data
 print "After Modify :\n";
 print "$languages{'England'}\n";
 print "$languages{'France'}\n";
