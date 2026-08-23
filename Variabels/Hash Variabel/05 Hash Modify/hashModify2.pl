bcb9a225adea4e3a2d872e5b36e49eb155a60ad2
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 14:46:08 +0700

    Update hashModify2.pl

--- Variabels/Hash Variabel/05 Hash Modify/hashModify2.pl
+++ Variabels/Hash Variabel/05 Hash Modify/hashModify2.pl
@@ -7,47 +7,47 @@ use strict;
 #2 - defines vocabulary => vocabulary hash
 
 my %vocab = (Knight => 'Never Give Up', Tinkle => 'The Beauty Of Life', Beast => 'We Are Warrior', Falcon => 'Stronger Than The Storm', Cowboy => 'Talk Slowly, Think Quickly');
-#my %languages = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
+#my %language = ( England => 'Inggris', France => 'Perancis', Spain => 'Spanyol', China => 'Cina');
 
 # get data
-my $language1 = $languages{'England'};
-my $language2 = $languages{'France'};
-my $language3 = $languages{'Spain'};
-my $language4 = $languages{'China'};
-#my $language1 = $languages{'England'};
-#my $language2 = $languages{'France'};
-#my $language3 = $languages{'Spain'};
-#my $language4 = $languages{'China'};
+my $vocab1 = $language{'England'};
+my $vocab2 = $language{'France'};
+my $vocab3 = $language{'Spain'};
+my $vocab4 = $language{'China'};
+#my $language1 = $language{'England'};
+#my $language2 = $language{'France'};
+#my $language3 = $language{'Spain'};
+#my $language4 = $language{'China'};
 
 # print data
-print($language1, "\n");
-print($language2, "\n");
-print($language3, "\n");
-print($language4, "\n");
+print($vocab1, "\n");
+print($vocab2, "\n");
+print($vocab3, "\n");
+print($vocab4, "\n");
 #print($language1, "\n");
 #print($language2, "\n");
 #print($language3, "\n");
 #print($language4, "\n");
 
 # Add New data
-$languages{'Germany'} = 'Jerman';
-$languages{'India'} = 'Pegal he.. Pegal he...';
-#$languages{'Germany'} = 'Jerman';
-#$languages{'India'} = 'Pegal he.. Pegal he...';
+$vocab{'Germany'} = 'Jerman';
+$vocab{'India'} = 'Pegal he.. Pegal he...';
+#$language{'Germany'} = 'Jerman';
+#$language{'India'} = 'Pegal he.. Pegal he...';
 
 # Modify data
-$languages{'India'} = 'Hindustan';
-#$languages{'India'} = 'Hindustan';
+$vocab{'India'} = 'Hindustan';
+#$language{'India'} = 'Hindustan';
 
 # get new data
-my $language5 = $languages{'Germany'};
-my $language6 = $languages{'India'};
-#my $language5 = $languages{'Germany'};
-#my $language6 = $languages{'India'};
+my $vocab5 = $languages{'Germany'};
+my $vocab6 = $languages{'India'};
+#my $language5 = $language{'Germany'};
+#my $language6 = $language{'India'};
 
 # print New data
-print($language5, "\n");
-print($language6, "\n");
+print($vocab5, "\n");
+print($vocab6, "\n");
 #print($language5, "\n");
 #print($language6, "\n");
 
