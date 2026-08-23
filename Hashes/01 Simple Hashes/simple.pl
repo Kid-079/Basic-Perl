e71e276aafff1c8a24dfb75968fe658f5a5215ee
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 13:31:59 +0700

    Update simple.pl

--- Hashes/01 Simple Hashes/simple.pl
+++ Hashes/01 Simple Hashes/simple.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 
-%kata = ('Knight', 38, 'Tinkle', 69, 'Coboy', 52, 'Falcon', 83, 'Spark', 43 );
+%kata = ('Knight', 38, 'Tinkle', 69, 'Cowboy', 52, 'Falcon', 83, 'Spark', 43 );
 #%kata = ('Ornament', 101, 'Lonely', 89, 'Wise', 104,'Grizzly',  111, 'Shoe', 78);
 
 print "\$kata{'Knight'} = $kata{'Knight'}\n";
 print "\$kata{'Tinkle'} = $kata{'Tinkle'}\n";
-print "\$kata{'Coboy'} = $kata{'Coboy'}\n";
+print "\$kata{'Coboy'} = $kata{'Cowboy'}\n";
 print "\$kata{'Falcon'} = $kata{'Falcon'}\n";
 print "\$kata{'Spark'} = $kata{'Spark'}\n";
 
