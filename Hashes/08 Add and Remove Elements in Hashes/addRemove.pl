04048c2eb83b1ceaf8a3760288d08f5134389d1c
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-23 13:34:24 +0700

    Update addRemove.pl

--- Hashes/08 Add and Remove Elements in Hashes/addRemove.pl
+++ Hashes/08 Add and Remove Elements in Hashes/addRemove.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 %data = ('Rumble' => 67, 'Mighty' => 48, 'Thunder' => 98, 'Meteor' => 79, 'Respect' => 89);
-#%data = ('Knight' => 52, 'Tinkle' => 83, 'Coboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
+#%data = ('Knight' => 52, 'Tinkle' => 83, 'Cowboy' => 116, 'Falcon' => 27, 'Spark' => 50, 'Beast' => 33, 'Fairy' => 90, 'Baboon' => 60);
 #%data = ('A' => 50, 'B' => 55, 'C' => 48, 'D' => 67);
 
 @keys = keys %data;
